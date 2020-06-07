9ea35413ba9fd12f579a651bd001850aa9f40c67
Oscar Freij <otg020313@gmail.com>
2020-06-07 04:23:38 +0200

    Added PathLength calculation and Path Progress for enemies

--- Assets/Resources/Scripts/Enemy.cs
+++ Assets/Resources/Scripts/Enemy.cs
@@ -7,6 +7,11 @@ public class Enemy : MonoBehaviour
 {
     public Transform Target { get; private set; }
     public float Speed { get; set; }
+    public float TimeAlive { get; private set; }
+
+    public float PathLength { get; private set; }
+    public float PathLengthTraveled { get; private set; }
+    public float PathProgress { get; private set; }
 
     public GameObject Path { get; private set; }
     public int PathItemNr { get; private set; }
@@ -18,13 +23,17 @@ public class Enemy : MonoBehaviour
     {
         try
         {
+            this.PathLength = Convert.ToInt32(Path.gameObject.name.Substring(Path.gameObject.name.IndexOf(':')+1));
+            this.PathLengthTraveled = 0;
+            this.PathProgress = 0;
+            this.TimeAlive = 0;
             this.Speed = Speed;
             this.Path = Path;
             this.transform.position = this.Path.transform.Find("start").transform.position;
             this.PathItemNr = 0;
             this.Target = this.Path.transform.Find(this.PathItemNr.ToString()).transform;
-            Debug.Log("Number of waypoints: " + this.Path.transform.childCount);
-            Debug.Log("Moving to waypoint: " + this.PathItemNr);
+            //Debug.Log("Number of waypoints: " + this.Path.transform.childCount);
+            //Debug.Log("Moving to waypoint: " + this.PathItemNr);
 
             this.HasBeenInitiated = true;
         }
@@ -41,11 +50,12 @@ public class Enemy : MonoBehaviour
     {
         if (this.HasBeenInitiated)
         {
+            this.TimeAlive += Time.deltaTime;
             if (this.Target.position == this.transform.position)
             {
                 if (this.Target.name == "finish")
                 {
-                    Debug.Log("Target reached!");
+                    //Debug.Log("Target reached!");
                     Destroy(this.gameObject);
                 }
 
@@ -54,24 +64,33 @@ public class Enemy : MonoBehaviour
                 if (this.PathItemNr == this.Path.transform.childCount)
                 {
                     this.Target = this.Path.transform.Find("finish").transform;
-                    Debug.Log("Heading to last waypoint!");
+                    //Debug.Log("Heading to last waypoint!");
                 }
                 else if (this.PathItemNr < this.Path.transform.childCount - 2)
                 {
                     this.Target = this.Path.transform.Find(this.PathItemNr.ToString()).transform;
-                    Debug.Log("Moving to waypoint: " + this.PathItemNr);
+                    //Debug.Log("Moving to waypoint: " + this.PathItemNr);
                 }
 
             }
             else
             {
                 float step = Speed * Time.deltaTime;
+                this.PathLengthTraveled += step;
                 this.transform.position = Vector3.MoveTowards(this.transform.position, Target.position, step);
+                this.PathProgress = (float)(Math.Round(this.PathLengthTraveled / this.PathLength, 3) * 100);
+                
+                if (this.PathProgress > 100)
+                {
+                    this.PathProgress = 100;
+                }
+
+                this.gameObject.name = "Enemy: " + this.PathProgress + "%";
             }
         }
         else
         {
-            Debug.Log("Enemy waiting to be initiated!");
+            //Debug.Log("Enemy waiting to be initiated!");
         }
     }
 }
--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> Paths { get; private set; }
+    public GameObject EnemyList { get; private set; }
     public float SpawnTimer { get; private set; }
     public float TimeSinceLastSpawn { get; private set; }
     // Start is called before the first frame update
@@ -13,12 +15,38 @@ public class GameManager : MonoBehaviour
         this.SpawnTimer = 0.5f;
         this.TimeSinceLastSpawn = 0;
         this.Paths = new List<GameObject>();
+        this.EnemyList = GameObject.Find("EnemyList");
 
         Debug.Log("=== Loading waypoint path/s ===");
-        foreach (Transform child in GameObject.Find("Map").transform)
+        foreach (Transform path in GameObject.Find("Map").transform)
         {
-            this.Paths.Add(child.gameObject);
-            Debug.Log(child.gameObject.name + " : Loaded");
+            this.Paths.Add(path.gameObject);
+            
+            List<Transform> pathList = new List<Transform>();
+
+            float pathLength = 0.0f;
+
+            foreach (Transform waypoint in path)
+            {
+                pathList.Add(waypoint);
+            }
+
+            for (int i = 0; i < pathList.Count; i++)
+            {
+                if ( i >= 1)
+                {
+                    pathLength += Mathf.Round(
+                        Mathf.Sqrt(
+                            Mathf.Pow(pathList[i].position.x - pathList[i - 1].position.x, 2)
+                            + Mathf.Pow(pathList[i].position.y - pathList[i - 1].position.y, 2)
+                            + Mathf.Pow(pathList[i].position.z - pathList[i - 1].position.z, 2)
+                            )
+                        );
+                }
+            }
+
+            path.name = path.name + ":" + pathLength.ToString();
+            Debug.Log(path.gameObject.name + " : Loaded");
         }
         Debug.Log("=== Finished waypoint path/s ===");
     }
@@ -45,7 +73,7 @@ public class GameManager : MonoBehaviour
         }
         else if (this.Paths.Count > 1)
         {
-            Path = this.Paths[Random.Range(0, Paths.Count)];
+            Path = this.Paths[UnityEngine.Random.Range(0, Paths.Count)];
         }
         else
         {
@@ -54,9 +82,10 @@ public class GameManager : MonoBehaviour
 
         GameObject NewEnemy = Instantiate(Resources.Load("Prefabs/Enemy"), new Vector3(0,0,0), Quaternion.identity) as GameObject;
 
-        if (NewEnemy.GetComponent<Enemy>().Init(Path, 10.0f))
+        if (NewEnemy.GetComponent<Enemy>().Init(Path, 5.0f))
         {
-            Debug.Log("Enemy initialized!");
+            NewEnemy.transform.parent = this.EnemyList.transform;
+            //Debug.Log("Enemy initialized!");
         }
         else
         {
