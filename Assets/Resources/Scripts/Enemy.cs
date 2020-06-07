3171d20d7b4eb275b5f5635180b36e23a4dbd476
Oscar Freij <otg020313@gmail.com>
2020-06-07 02:44:39 +0200

    Waypoint-based movement for enemies

---
+++ Assets/Resources/Scripts/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform Target { get; private set; }
+    public float Speed { get; set; }
+
+    public GameObject Path { get; private set; }
+    public int PathItemNr { get; private set; }
+
+    public bool PathIsSelected { get; private set; }
+    public bool HasBeenInitiated;
+
+    public bool Init(GameObject Path, float Speed)
+    {
+        try
+        {
+            this.Speed = Speed;
+            this.Path = Path;
+            this.transform.position = this.Path.transform.Find("start").transform.position;
+            this.PathItemNr = 0;
+            this.Target = this.Path.transform.Find(this.PathItemNr.ToString()).transform;
+            Debug.Log("Number of waypoints: " + this.Path.transform.childCount);
+            Debug.Log("Moving to waypoint: " + this.PathItemNr);
+
+            this.HasBeenInitiated = true;
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.HasBeenInitiated)
+        {
+            if (this.Target.position == this.transform.position)
+            {
+                if (this.Target.name == "finish")
+                {
+                    Debug.Log("Target reached!");
+                    Destroy(this.gameObject);
+                }
+
+                this.PathItemNr++;
+
+                if (this.PathItemNr == this.Path.transform.childCount)
+                {
+                    this.Target = this.Path.transform.Find("finish").transform;
+                    Debug.Log("Heading to last waypoint!");
+                }
+                else if (this.PathItemNr < this.Path.transform.childCount - 2)
+                {
+                    this.Target = this.Path.transform.Find(this.PathItemNr.ToString()).transform;
+                    Debug.Log("Moving to waypoint: " + this.PathItemNr);
+                }
+
+            }
+            else
+            {
+                float step = Speed * Time.deltaTime;
+                this.transform.position = Vector3.MoveTowards(this.transform.position, Target.position, step);
+            }
+        }
+        else
+        {
+            Debug.Log("Enemy waiting to be initiated!");
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> Paths { get; private set; }
+    public float SpawnTimer { get; private set; }
+    public float TimeSinceLastSpawn { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.SpawnTimer = 0.5f;
+        this.TimeSinceLastSpawn = 0;
+        this.Paths = new List<GameObject>();
+
+        Debug.Log("=== Loading waypoint path/s ===");
+        foreach (Transform child in GameObject.Find("Map").transform)
+        {
+            this.Paths.Add(child.gameObject);
+            Debug.Log(child.gameObject.name + " : Loaded");
+        }
+        Debug.Log("=== Finished waypoint path/s ===");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.TimeSinceLastSpawn += Time.deltaTime;
+        if (this.TimeSinceLastSpawn >= this.SpawnTimer)
+        {
+            SpawnEnemy();
+            this.TimeSinceLastSpawn = 0;
+        }
+    }
+
+    public bool SpawnEnemy()
+    {
+        GameObject Path;
+
+        if (this.Paths.Count == 0)
+        {
+            Debug.LogError("ERROR: No viable paths found for enemy");
+            return false;
+        }
+        else if (this.Paths.Count > 1)
+        {
+            Path = this.Paths[Random.Range(0, Paths.Count)];
+        }
+        else
+        {
+            Path = this.Paths[0];
+        }
+
+        GameObject NewEnemy = Instantiate(Resources.Load("Prefabs/Enemy"), new Vector3(0,0,0), Quaternion.identity) as GameObject;
+
+        if (NewEnemy.GetComponent<Enemy>().Init(Path, 10.0f))
+        {
+            Debug.Log("Enemy initialized!");
+        }
+        else
+        {
+            Debug.LogError("ERROR: Enemy Faild to initialize!");
+        }
+
+        return true;
+    }
+}
